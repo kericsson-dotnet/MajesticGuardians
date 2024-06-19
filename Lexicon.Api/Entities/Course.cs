1cb86695f8c34933a95a0f155d6cf99ec9b52917
Karl Ericsson <lexicon@services.karlericsson.com>
2024-06-19 13:53:37 +0200

    Add entities

---
+++ Lexicon.Api/Entities/Course.cs
@@ -0,0 +1,13 @@
+namespace Lexicon.Api.Entities;
+
+public class Course
+{
+    public int CourseId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public List<User> Users { get; set; } = [];
+    public DateTime StartDate { get; set; } = DateTime.Now;
+    public DateTime EndDate { get; set; }
+    public List<Document> Documents { get; set; } = [];
+}
+
