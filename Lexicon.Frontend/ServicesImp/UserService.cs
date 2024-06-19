d1207ed7fca92707df5b5a8f9e6c644ff5f1ccb4
98emre <emre-98@live.se>
2024-06-19 17:03:45 +0200

    feat: added implentation unit of work and user service

---
+++ Lexicon.Frontend/ServicesImp/UserService.cs
@@ -0,0 +1,17 @@
+﻿using Lexicon.Frontend.Models;
+using Lexicon.Frontend.Services;
+
+namespace Lexicon.Frontend.ServicesImp
+{
+    public class UserService : IUserService
+    {
+        private readonly HttpClient _httpClient;
+
+        public UserService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IEnumerable<User>> GetUsersAsync() => await _httpClient.GetFromJsonAsync<IEnumerable<User>>("api/users");
+    }
+}
