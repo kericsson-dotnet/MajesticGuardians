d9f9c794382eb9ccbc2e8bc5382739cf859c7fb1
98emre <emre-98@live.se>
2024-06-20 14:05:21 +0200

    feat: added user dtos and added the user dtos to mapper

---
+++ Lexicon.Api/Dtos/UserDtos/UserDto.cs
@@ -0,0 +1,21 @@
+﻿using Lexicon.Api.Entities;
+
+namespace Lexicon.Api.Dtos.UserDtos
+{
+    public class UserDto
+    {
+        public int UserId { get; set; }
+        
+        public string FirstName { get; set; } = string.Empty;
+        
+        public string LastName { get; set; } = string.Empty;
+        
+        public string Email { get; set; } = string.Empty;
+        
+        public string Password { get; set; } = string.Empty;
+
+        public UserRole Role { get; set; } = UserRole.Student;
+
+        public List<Document>? Documents { get; set; } = [];
+    }
+}
