ac46332bb84294db346c4bacb9d8bf8c1ad37090
98emre <emre-98@live.se>
2024-06-20 14:08:52 +0200

    feat: added courses dtos and added the dtos to the mapper

---
+++ Lexicon.Api/Dtos/CourseDtos/CoursePostDto.cs
@@ -0,0 +1,15 @@
+﻿using Lexicon.Api.Entities;
+
+namespace Lexicon.Api.Dtos.CourseDtos
+{
+    public class CoursePostDto
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public DateTime StartDate { get; set; } = DateTime.Now;
+
+        public DateTime EndDate { get; set; }
+    }
+}
