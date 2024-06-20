44d0b5693ec266a19faab7c92f5ac0c3483ff73c
98emre <emre-98@live.se>
2024-06-20 11:49:23 +0200

    feat: added repository for document

---
+++ Lexicon.Api/Repositories/IDocumentRepository.cs
@@ -0,0 +1,10 @@
+﻿using Lexicon.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lexicon.Api.Repositories
+{
+    public interface IDocumentRepository : ICrudRepository<Document>
+    {
+
+    }
+}
