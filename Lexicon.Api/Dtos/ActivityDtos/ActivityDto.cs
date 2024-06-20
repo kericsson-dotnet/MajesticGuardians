1505b2700beae475838c0aa490284a563ba072c7
98emre <emre-98@live.se>
2024-06-20 14:11:10 +0200

    feat: added activity dtos and added dtos to the mapper

---
+++ Lexicon.Api/Dtos/ActivityDtos/ActivityDto.cs
@@ -0,0 +1,19 @@
+﻿using Lexicon.Api.Entities;
+
+namespace Lexicon.Api.Dtos.ActivityDtos
+{
+    public class ActivityDto
+    {
+        public int ActivityId { get; set; }
+        
+        public ActivityType Type { get; set; }
+        
+        public string Name { get; set; } = string.Empty;
+        
+        public DateTime StartDate { get; set; } = DateTime.Now;
+        
+        public DateTime EndDate { get; set; }
+        
+        public List<Document> Documents { get; set; } = [];
+    }
+}
---
+++ Lexicon.Api/Dtos/ActivityDtos/ActivityPostDto.cs
@@ -0,0 +1,15 @@
+﻿using Lexicon.Api.Entities;
+
+namespace Lexicon.Api.Dtos.ActivityDtos
+{
+    public class ActivityPostDto
+    {
+        public ActivityType Type { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public DateTime StartDate { get; set; } = DateTime.Now;
+
+        public DateTime EndDate { get; set; }
+    }
+}
