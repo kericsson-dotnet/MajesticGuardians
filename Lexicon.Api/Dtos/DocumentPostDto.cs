5044a0c28b4c249814501c4046a6eed875428101
98emre <emre-98@live.se>
2024-06-20 09:33:46 +0200

    feat: created document post dto

---
+++ Lexicon.Api/Dtos/DocumentPostDto.cs
@@ -0,0 +1,15 @@
+﻿using Lexicon.Api.Entities;
+
+namespace Lexicon.Api.Dtos
+{
+    public class DocumentPostDto
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public string Url { get; set; } = string.Empty;
+
+        public DateTime TimeAdded { get; set; } = DateTime.Now;
+    }
+}
