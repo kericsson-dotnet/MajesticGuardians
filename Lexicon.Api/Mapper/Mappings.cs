a2106f0af4dc338adf5dfb2fc7981de07fcca9fa
98emre <emre-98@live.se>
2024-06-20 09:45:54 +0200

    feat: create mapper class and added mapping for document

---
+++ Lexicon.Api/Mapper/Mappings.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Lexicon.Api.Dtos.DocumentDtos;
+using Lexicon.Api.Entities;
+
+namespace Lexicon.Api.Mapper
+{
+    public class Mappings : Profile
+    {
+        public Mappings()
+        {
+            // Document mapper
+            CreateMap<DocumentDto, Document>();
+            CreateMap<Document, DocumentDto>();
+            CreateMap<Document, DocumentPostDto>();
+            CreateMap<DocumentPostDto, Document>();
+        }
+
+    }
+}
