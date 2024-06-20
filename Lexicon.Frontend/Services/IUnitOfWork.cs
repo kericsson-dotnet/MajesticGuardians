45f09f0efe9437b91bee26b8fb16afc3eeddd406
FelixEdenborgh <Felix@edenborgh.se>
2024-06-20 13:06:13 +0200

    Fixed so that courses blazor page now works, and shows all courses.

--- Lexicon.Frontend/Services/IUnitOfWork.cs
+++ Lexicon.Frontend/Services/IUnitOfWork.cs
@@ -1,8 +1,11 @@
-﻿namespace Lexicon.Frontend.Services
+﻿using Microsoft.AspNetCore.Cors.Infrastructure;
+
+namespace Lexicon.Frontend.Services
 {
     public interface IUnitOfWork
     {
         IUserService UserService { get; }
         IAuthService AuthService { get; }
+        ICourseService CourseService { get; }
     }
 }
