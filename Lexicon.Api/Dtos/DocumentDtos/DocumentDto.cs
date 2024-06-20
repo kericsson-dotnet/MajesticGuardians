102820dae6a39f27a75b329d60489993cd71c911
98emre <emre-98@live.se>
2024-06-20 09:34:26 +0200

    feat: added document dtos folder

--- Lexicon.Api/Dtos/DocumentDtos/DocumentDto.cs
+++ Lexicon.Api/Dtos/DocumentDtos/DocumentDto.cs
@@ -1,19 +1,19 @@
 ﻿using Lexicon.Api.Entities;
 
-namespace Lexicon.Api.Dtos
+namespace Lexicon.Api.Dtos.DocumentDtos
 {
     public class DocumentDto
     {
         public int DocumentId { get; set; }
 
         public string Name { get; set; } = string.Empty;
-        
+
         public string Description { get; set; } = string.Empty;
-        
+
         public User? AddedBy { get; set; }
-        
+
         public string Url { get; set; } = string.Empty;
-        
+
         public DateTime TimeAdded { get; set; } = DateTime.Now;
     }
 }
