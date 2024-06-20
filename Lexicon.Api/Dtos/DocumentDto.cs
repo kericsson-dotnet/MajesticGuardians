dc3412b7afa4d65b9f60bb645cd051be6291ec5c
98emre <emre-98@live.se>
2024-06-20 09:33:36 +0200

    feat: created document dto

---
+++ Lexicon.Api/Dtos/DocumentDto.cs
@@ -0,0 +1,19 @@
+﻿using Lexicon.Api.Entities;
+
+namespace Lexicon.Api.Dtos
+{
+    public class DocumentDto
+    {
+        public int DocumentId { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+        
+        public string Description { get; set; } = string.Empty;
+        
+        public User? AddedBy { get; set; }
+        
+        public string Url { get; set; } = string.Empty;
+        
+        public DateTime TimeAdded { get; set; } = DateTime.Now;
+    }
+}
