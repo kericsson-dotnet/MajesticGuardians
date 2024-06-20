4e9bb6d9a02fcdfd2b47237245c206ca989ebdb9
98emre <emre-98@live.se>
2024-06-20 14:13:59 +0200

    feat: added moduel dtos and added dtos to the mapper

---
+++ Lexicon.Api/Dtos/ModuleDtos/ModuleDto.cs
@@ -0,0 +1,23 @@
+﻿using Lexicon.Api.Entities;
+
+namespace Lexicon.Api.Dtos.ModuleDtos
+{
+    public class ModuleDto
+    {
+        public int ModuleId { get; set; }
+
+        public List<Activity> Activities { get; set; } = [];
+       
+        public Course? Course { get; set; }
+        
+        public string Name { get; set; } = string.Empty;
+        
+        public string Description { get; set; } = string.Empty;
+        
+        public DateTime StartDate { get; set; } = DateTime.Now;
+        
+        public DateTime EndDate { get; set; }
+        
+        public List<Document> Documents { get; set; } = [];
+    }
+}
