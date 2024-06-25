c7b62bc31ad654e371643149767a1ff893d6d8bd
fionaliu66 <fiona.liu0606@gmail.com>
2024-06-25 09:06:21 +0200

    Rebase

--- Lexicon.Api/Controllers/LoginController.cs
+++ Lexicon.Api/Controllers/LoginController.cs
@@ -1,6 +1,7 @@
 ﻿using Lexicon.Api.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Lexicon.Api.Models;
+using Lexicon.Api.Services;
 
 namespace Lexicon.Api.Controllers
 {
@@ -8,10 +9,13 @@ namespace Lexicon.Api.Controllers
     public class LoginController : Controller
     {
         private readonly IUserRepository _userRepository;
+        private readonly IConfiguration _configuration;
+        private readonly TokenService _tokenService;
 
-        public LoginController(IUserRepository userRepository)
+        public LoginController(IUserRepository userRepository,IConfiguration configuration)
         {
             _userRepository = userRepository;
+            _tokenService = new TokenService(configuration);
         }
 
         [HttpPost("validate")]
@@ -19,15 +23,20 @@ namespace Lexicon.Api.Controllers
         {
             var user = await _userRepository.ValidateCredentialsAsync(model.Email, model.Password);
             if (user == null)
+            {           
+                return Unauthorized();
+            }
+            string token;
+            if (user.Role == Entities.UserRole.Teacher)
             {
-                Console.WriteLine("Cannot find the user");
-                return Ok(false); 
+                token = _tokenService.GenerateToken(model.Email, "Admin");
             }
             else
             {
-                Console.WriteLine(" find the user");
-                return Ok(true); 
+                token = _tokenService.GenerateToken(model.Email, "User");
             }
+       
+            return Ok(new { Token = token });
 
         }
 
---
+++ Lexicon.Api/Services/TokenService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Lexicon.Api.Services
+{
+    public class TokenService
+    {
+        private readonly IConfiguration _configuration;
+
+        public TokenService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateToken(string username, string role)
+        {
+            var key = Encoding.ASCII.GetBytes(_configuration["Jwt:Key"]);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                new Claim(ClaimTypes.Name, username),
+                new Claim(ClaimTypes.Role, role)
+            }),
+                Expires = DateTime.UtcNow.AddHours(1),
+                Issuer = _configuration["Jwt:Issuer"],
+                Audience = _configuration["Jwt:Audience"],
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+
+    }
+}
