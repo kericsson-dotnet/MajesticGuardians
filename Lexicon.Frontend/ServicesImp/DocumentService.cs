6360c4cc53827e405b6cf84039aacd4cbcf8c774
Karl Ericsson <lexicon@services.karlericsson.com>
2024-06-25 15:33:16 +0200

    feat(frontend): Add document model and service

---
+++ Lexicon.Frontend/ServicesImp/DocumentService.cs
@@ -0,0 +1,16 @@
+﻿using Lexicon.Frontend.Models;
+using Lexicon.Frontend.Services;
+
+namespace Lexicon.Frontend.ServicesImp;
+
+public class DocumentService : IDocumentService
+{
+    private readonly HttpClient _httpClient;
+
+    public DocumentService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<IEnumerable<Document>> GetDocumentsAsync() => await _httpClient.GetFromJsonAsync<IEnumerable<Document>>("api/documents");
+}
