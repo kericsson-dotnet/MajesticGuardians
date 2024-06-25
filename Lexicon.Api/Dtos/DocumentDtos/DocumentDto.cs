756a6db0cf5405bc8e27f0203cd9cd2f28fdd2d0
98emre <emre-98@live.se>
2024-06-25 09:41:00 +0200

    feat: added document annotation dtos

--- Lexicon.Api/Dtos/DocumentDtos/DocumentDto.cs
+++ Lexicon.Api/Dtos/DocumentDtos/DocumentDto.cs
@@ -1,19 +1,29 @@
 ﻿using Lexicon.Api.Entities;
+using System.ComponentModel.DataAnnotations;
 
 namespace Lexicon.Api.Dtos.DocumentDtos
 {
     public class DocumentDto
     {
+        [Key]
         public int DocumentId { get; set; }
 
+        [Required]
+        [StringLength(100, ErrorMessage = "Name length can't be more than 100.")]
         public string Name { get; set; } = string.Empty;
 
+        [Required]
+        [StringLength(500, ErrorMessage = "Description length can't be more than 500.")]
         public string Description { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(500, ErrorMessage = "Url length can't be more than 500.")]
+        public string Url { get; set; } = string.Empty;
+
+        [Required]
+        public DateTime TimeAdded { get; set; } = DateTime.Now;
 
         public User? AddedBy { get; set; }
 
-        public string Url { get; set; } = string.Empty;
-
-        public DateTime TimeAdded { get; set; } = DateTime.Now;
     }
 }
