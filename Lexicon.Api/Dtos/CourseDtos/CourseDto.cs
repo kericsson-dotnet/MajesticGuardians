89f47044f14e47a72bd77f22658d3811489069bb
98emre <emre-98@live.se>
2024-06-25 09:37:56 +0200

    feat: added annotations for course dtos

--- Lexicon.Api/Dtos/CourseDtos/CourseDto.cs
+++ Lexicon.Api/Dtos/CourseDtos/CourseDto.cs
@@ -1,20 +1,28 @@
 ﻿using Lexicon.Api.Entities;
+using System.ComponentModel.DataAnnotations;
 
 namespace Lexicon.Api.Dtos.CourseDtos
 {
     public class CourseDto
     {
+        [Key]
         public int CourseId { get; set; }
 
+        [Required]
+        [StringLength(100, ErrorMessage = "Name length can't be more than 100.")]
         public string Name { get; set; } = string.Empty;
 
+        [Required]
+        [StringLength(500, ErrorMessage = "Description length can't be more than 100.")]
         public string Description { get; set; } = string.Empty;
 
-        public List<User> Users { get; set; } = [];
-
+        [Required]
         public DateTime StartDate { get; set; } = DateTime.Now;
 
+        [Required]
         public DateTime EndDate { get; set; }
+
+        public List<User> Users { get; set; } = [];
 
         public List<Document> Documents { get; set; } = [];
     }
--- Lexicon.Api/Dtos/CourseDtos/CoursePostDto.cs
+++ Lexicon.Api/Dtos/CourseDtos/CoursePostDto.cs
@@ -1,15 +1,22 @@
 ﻿using Lexicon.Api.Entities;
+using System.ComponentModel.DataAnnotations;
 
 namespace Lexicon.Api.Dtos.CourseDtos
 {
     public class CoursePostDto
     {
+        [Required]
+        [StringLength(100, ErrorMessage = "Name length can't be more than 100.")]
         public string Name { get; set; } = string.Empty;
 
+        [Required]
+        [StringLength(500, ErrorMessage = "Description length can't be more than 100.")]
         public string Description { get; set; } = string.Empty;
 
+        [Required]
         public DateTime StartDate { get; set; } = DateTime.Now;
 
+        [Required]
         public DateTime EndDate { get; set; }
     }
 }
