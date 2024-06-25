f223a9b51086bd71808e911a6106064ab69a978f
98emre <emre-98@live.se>
2024-06-25 09:35:05 +0200

    feat: added annotation for activity dtos

--- Lexicon.Api/Dtos/ActivityDtos/ActivityDto.cs
+++ Lexicon.Api/Dtos/ActivityDtos/ActivityDto.cs
@@ -1,17 +1,24 @@
 ﻿using Lexicon.Api.Entities;
+using System.ComponentModel.DataAnnotations;
 
 namespace Lexicon.Api.Dtos.ActivityDtos
 {
     public class ActivityDto
     {
+        [Key]
         public int ActivityId { get; set; }
-        
+
+        [Required]
         public ActivityType Type { get; set; }
-        
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Name length can't be more than 100.")]
         public string Name { get; set; } = string.Empty;
-        
+
+        [Required]
         public DateTime StartDate { get; set; } = DateTime.Now;
-        
+
+        [Required]
         public DateTime EndDate { get; set; }
         
         public List<Document> Documents { get; set; } = [];
