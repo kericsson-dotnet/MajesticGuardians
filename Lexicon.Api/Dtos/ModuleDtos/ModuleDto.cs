bddd10b14905f4a23327fca10cd32e040f63d26f
98emre <emre-98@live.se>
2024-06-25 09:43:27 +0200

    feat: added annotations for module dtos

--- Lexicon.Api/Dtos/ModuleDtos/ModuleDto.cs
+++ Lexicon.Api/Dtos/ModuleDtos/ModuleDto.cs
@@ -1,23 +1,31 @@
 ﻿using Lexicon.Api.Entities;
+using System.ComponentModel.DataAnnotations;
 
 namespace Lexicon.Api.Dtos.ModuleDtos
 {
     public class ModuleDto
     {
+        [Key]
         public int ModuleId { get; set; }
 
-        public List<Activity> Activities { get; set; } = [];
-       
-        public Course? Course { get; set; }
-        
+        [Required]
+        [StringLength(100, ErrorMessage = "Name length can't be more than 100.")]
         public string Name { get; set; } = string.Empty;
-        
+
+        [Required]
+        [StringLength(500, ErrorMessage = "Description length can't be more than 500.")]
         public string Description { get; set; } = string.Empty;
-        
+
+        [Required]
         public DateTime StartDate { get; set; } = DateTime.Now;
-        
+
+        [Required]
         public DateTime EndDate { get; set; }
-        
+
+        public Course? Course { get; set; }
+
+        public List<Activity> Activities { get; set; } = [];
+
         public List<Document> Documents { get; set; } = [];
     }
 }
--- Lexicon.Api/Dtos/ModuleDtos/ModulePostDto.cs
+++ Lexicon.Api/Dtos/ModuleDtos/ModulePostDto.cs
@@ -1,13 +1,21 @@
-﻿namespace Lexicon.Api.Dtos.ModuleDtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lexicon.Api.Dtos.ModuleDtos
 {
     public class ModulePostDto
     {
+        [Required]
+        [StringLength(100, ErrorMessage = "Name length can't be more than 100.")]
         public string Name { get; set; } = string.Empty;
 
+        [Required]
+        [StringLength(100, ErrorMessage = "Description length can't be more than 500.")]
         public string Description { get; set; } = string.Empty;
 
+        [Required]
         public DateTime StartDate { get; set; } = DateTime.Now;
 
+        [Required]
         public DateTime EndDate { get; set; }
     }
 }
