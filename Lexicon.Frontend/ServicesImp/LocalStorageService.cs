dcdd89c8da3e3a078a5ad844fc3e6189f47903a0
98emre <emre-98@live.se>
2024-06-26 10:45:14 +0200

    feat: added remaining code for auth in frontend, where token is save at the local storage

---
+++ Lexicon.Frontend/ServicesImp/LocalStorageService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.JSInterop;
+
+namespace Lexicon.Frontend.ServicesImp
+{
+    public class LocalStorageService
+    {
+        private readonly IJSRuntime _jsRuntime;
+
+        public LocalStorageService(IJSRuntime jsRuntime)
+        {
+            _jsRuntime = jsRuntime;
+        }
+
+        public async Task SetItemAsync(string key, string value)
+        {
+            await _jsRuntime.InvokeVoidAsync("localStorage.setItem", key, value);
+        }
+
+        public async Task<string> GetItemAsync(string key)
+        {
+            return await _jsRuntime.InvokeAsync<string>("localStorage.getItem", key);
+        }
+
+        public async Task RemoveItemAsync(string key)
+        {
+            await _jsRuntime.InvokeVoidAsync("localStorage.removeItem", key);
+        }
+    }
+}
