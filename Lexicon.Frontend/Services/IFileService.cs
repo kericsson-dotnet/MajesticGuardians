81ebad0eb37d02da5231a9ad9355de4893a5cb7e
fionaliu66 <fiona.liu0606@gmail.com>
2024-06-26 16:45:01 +0200

    File upload logic part. API done

---
+++ Lexicon.Frontend/Services/IFileService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lexicon.Frontend.Services
+{
+	public interface IFileService
+	{
+		Task<string> PostFileAsync(MultipartFormDataContent content);
+	}
+}
