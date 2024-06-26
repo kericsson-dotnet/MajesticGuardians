077c68250ef9f1745d81782bdf6418b0ef215d3e
98emre <emre-98@live.se>
2024-06-26 09:49:06 +0200

    feat: added code for generated token when loggin

---
+++ Lexicon.Api/Services/TokenService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Lexicon.Api.Services
+{
+    public class TokenService
+    {
+        private readonly IConfiguration _configuration;
+
+        public TokenService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateToken(string email, string role)
+        {
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub,email ),
+                new Claim(ClaimTypes.Role, role),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+            var key = Encoding.ASCII.GetBytes(_configuration["Jwt:Key"]);
+            var creds = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature);
+            var token = new JwtSecurityToken(
+               _configuration["Jwt:Issuer"],
+               _configuration["Jwt:Audience"],
+               claims,
+               expires: DateTime.Now.AddMinutes(30),
+               signingCredentials: creds);
+
+            // var tokenHandler = new JwtSecurityTokenHandler();
+            //var token = tokenHandler.CreateToken(tokenDescriptor);
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+    }
+}
