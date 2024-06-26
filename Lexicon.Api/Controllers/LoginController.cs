e5fcd967fdd830db687790aa6c678a0b841a94b9
98emre <emre-98@live.se>
2024-06-26 09:49:06 +0200

    feat: added code for generated token when loggin

--- Lexicon.Api/Controllers/LoginController.cs
+++ Lexicon.Api/Controllers/LoginController.cs
@@ -1,6 +1,7 @@
 ﻿using Lexicon.Api.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Lexicon.Api.Models;
+using Lexicon.Api.Services;
 
 namespace Lexicon.Api.Controllers
 {
@@ -8,27 +9,35 @@ namespace Lexicon.Api.Controllers
     public class LoginController : Controller
     {
         private readonly IUserRepository _userRepository;
+        private readonly IConfiguration _configuration;
+        private readonly TokenService _tokenService;
 
-        public LoginController(IUserRepository userRepository)
+        public LoginController(IUserRepository userRepository, IConfiguration configuration)
         {
             _userRepository = userRepository;
+            _tokenService = new TokenService(configuration);
         }
 
         [HttpPost("validate")]
-        public async Task<ActionResult<bool>> ValidateUser([FromBody] UserLoginModel model)
+        public async Task<ActionResult> ValidateUser([FromBody] UserLoginModel model)
         {
             var user = await _userRepository.ValidateCredentialsAsync(model.Email, model.Password);
             if (user == null)
             {
-                Console.WriteLine("Cannot find the user");
-                return Ok(false); 
+                return Unauthorized();
+            }
+
+            string token;
+            if (user.Role == Entities.UserRole.Teacher)
+            {
+                token = _tokenService.GenerateToken(model.Email, user.Role.ToString());
             }
             else
             {
-                Console.WriteLine(" find the user");
-                return Ok(true); 
+                token = _tokenService.GenerateToken(model.Email, user.Role.ToString());
             }
 
+            return Ok(token);
         }
 
     }
