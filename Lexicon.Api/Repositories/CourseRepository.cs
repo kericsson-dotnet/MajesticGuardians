0cf1f3f1e422f3d58294822e2b6ed080e1e46b00
98emre <emre-98@live.se>
2024-06-28 14:41:30 +0200

    feat: added course repository and user post with id

---
+++ Lexicon.Api/Repositories/CourseRepository.cs
@@ -0,0 +1,26 @@
+﻿using Lexicon.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lexicon.Api.Repositories
+{
+    public class CourseRepository : CrudRepository<Course> ,ICourseRepository
+    {
+        public CourseRepository(DbContext context) : base(context)
+        {
+
+        }
+
+        public void AddUserToCourse(int courseId, User user)
+        {
+            //var course = _context.Set<Course>()
+            //                .Include(c => c.Users)
+            //                .FirstOrDefault(c => c.CourseId == courseId);
+
+            //if (course != null) 
+            //{
+            //    course.Users.Add(user);
+            //    _context.SaveChanges(); 
+            //}
+        }
+    }
+}
---
+++ Lexicon.Api/Repositories/ICourseRepository.cs
@@ -0,0 +1,9 @@
+﻿using Lexicon.Api.Entities;
+
+namespace Lexicon.Api.Repositories
+{
+    public interface ICourseRepository : ICrudRepository<Course>
+    {
+        void AddUserToCourse(int courseId, User user);
+    }
+}
