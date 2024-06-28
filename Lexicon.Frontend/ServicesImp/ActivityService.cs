5ba299d8c6e7c01d716f6f9e8840e139c3992b50
98emre <emre-98@live.se>
2024-06-28 10:12:25 +0200

    feat: added token request

--- Lexicon.Frontend/ServicesImp/ActivityService.cs
+++ Lexicon.Frontend/ServicesImp/ActivityService.cs
@@ -16,7 +16,8 @@ public class ActivityService : IActivityService
 
     public async Task<IEnumerable<Activity>> GetActivitiesAsync() => await _httpClient.GetFromJsonAsync<IEnumerable<Activity>>("api/activities");
     public async Task<Activity> GetActivityAsync(int id) => await _httpClient.GetFromJsonAsync<Activity>($"api/activities/{id}");
-    public async Task<bool> PutActivityAsync(int id, Activity activity) { 
+    public async Task<bool> PutActivityAsync(int id, Activity activity) 
+    { 
         var res = await _httpClient.PutAsJsonAsync($"api/activities/{id}", activity);
         return res.IsSuccessStatusCode;
 	}
