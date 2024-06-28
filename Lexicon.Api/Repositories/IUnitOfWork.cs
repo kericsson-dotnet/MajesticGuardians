d9553d76c98219330703c6e3168d9c8acc090b85
98emre <emre-98@live.se>
2024-06-28 14:42:49 +0200

    feat: added course repository to unit of work

--- Lexicon.Api/Repositories/IUnitOfWork.cs
+++ Lexicon.Api/Repositories/IUnitOfWork.cs
@@ -5,7 +5,7 @@ namespace Lexicon.Api.Repositories;
 public interface IUnitOfWork
 {
     ICrudRepository<User> Users { get; }
-    ICrudRepository<Course> Courses { get; }
+    ICourseRepository Courses { get; }
     ICrudRepository<Module> Modules { get; }
     ICrudRepository<Activity> Activities { get; }
     IDocumentRepository Documents { get; }
