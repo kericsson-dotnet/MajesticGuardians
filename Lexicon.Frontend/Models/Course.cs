0e978ece06c2439926aa1598d09ae7a0372019b6
Abed Fetrat <abedfetrat@gmail.com>
2024-06-28 17:27:33 +0200

    1. replace dummy user with logged in user 2. check for authorization 3. list all courses belonging to the user

--- Lexicon.Frontend/Models/Course.cs
+++ Lexicon.Frontend/Models/Course.cs
@@ -6,6 +6,7 @@
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public List<User> Users { get; set; } = new List<User>();
+        public List<int> UserIds { get; set; } = new List<int>();
         public DateTime StartDate { get; set; } = DateTime.Now;
         public DateTime EndDate { get; set; }
     }
