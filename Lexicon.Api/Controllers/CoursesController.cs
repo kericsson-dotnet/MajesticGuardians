78e056ff3de71d0928aeb331fab13f9bc82cf083
98emre <emre-98@live.se>
2024-06-28 14:43:07 +0200

    feat: added end points in course controller so you can add user to courses

--- Lexicon.Api/Controllers/CoursesController.cs
+++ Lexicon.Api/Controllers/CoursesController.cs
@@ -4,6 +4,7 @@ using Lexicon.Api.Entities;
 using Lexicon.Api.Repositories;
 using AutoMapper;
 using Lexicon.Api.Dtos.CourseDtos;
+using Lexicon.Api.Dtos.UserDtos;
 
 namespace Lexicon.Api.Controllers;
 
@@ -133,6 +134,41 @@ public class CoursesController : ControllerBase
 
         return CreatedAtAction("GetCourse", new { id = course.CourseId }, course);
     }
+
+    [HttpPost("{id}/addUserToCourse")]
+    public async Task<IActionResult> AddUserToCourse([FromRoute] int id, [FromBody] UserPostWithIdDto userPostWithIdDto)
+    {
+        try
+        {
+            var course = await _UoW.Courses.GetAsync(id);
+
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _UoW.Users.GetAsync(userPostWithIdDto.Id);
+
+            if (user == null)
+            {
+                return NotFound($"User with id {userPostWithIdDto.Id} not found.");
+            }
+
+            _UoW.Courses.AddUserToCourse(id, user);
+            await _UoW.SaveAsync();
+
+            return Ok($"User with id {userPostWithIdDto.Id} successfully added to course {id}.");
+        }
+        catch (InvalidOperationException ex)
+        {
+            return NotFound(ex.Message);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex.Message);
+        }
+    }
+
 
     [HttpDelete("{id}")]
     public async Task<IActionResult> DeleteCourse([FromRoute] int id)
