baa4f24b0c746b8aa0db1577cb53a071592bfe3d
98emre <emre-98@live.se>
2024-06-29 11:05:40 +0200

    feat: added logic to check if student already exist in a course

--- Lexicon.Api/Repositories/CourseRepository.cs
+++ Lexicon.Api/Repositories/CourseRepository.cs
@@ -19,6 +19,21 @@ namespace Lexicon.Api.Repositories
 
         public void AddUserToCourse(int courseId, User user)
         {
+
+            if (user.Role == UserRole.Student)
+            {
+
+                var isUserInAnyCourse = _context.Set<Course>()
+                                            .Include(c => c.Users)
+                                            .Any(c => c.Users.Any(u => u.UserId == user.UserId));
+
+                if(isUserInAnyCourse)
+                {
+                    throw new InvalidOperationException("Student is already register in a course");
+                }
+
+            }
+
             var course = _context.Set<Course>()
                             .Include(c => c.Users)
                             .FirstOrDefault(c => c.CourseId == courseId);
