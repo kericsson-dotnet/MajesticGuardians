981f7336b8ae5fce2797018b6942486217e92710
Karl Ericsson <lexicon@services.karlericsson.com>
2024-06-30 17:44:30 +0200

    fix(api): Update migrations

--- Lexicon.Api/Migrations/20240630150317_Init.Designer.cs
+++ Lexicon.Api/Migrations/20240630150317_Init.Designer.cs
@@ -4,6 +4,7 @@ using Lexicon.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Lexicon.Api.Migrations
 {
     [DbContext(typeof(LexiconLmsContext))]
-    partial class LexiconLmsContextModelSnapshot : ModelSnapshot
+    [Migration("20240630150317_Init")]
+    partial class Init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
