2bb98e9ab031ee258abe32e46a227e73e431c893
98emre <emre-98@live.se>
2024-06-30 10:54:10 +0200

    fix: did many to many relation between course and users. And now teacher can be in many courses.

--- Lexicon.Api/Entities/User.cs
+++ Lexicon.Api/Entities/User.cs
@@ -15,6 +15,8 @@ public class User
     public string Password { get; set; } = string.Empty;
     public UserRole Role { get; set; } = UserRole.Student;
     public List<Document>? Documents { get; set; } = [];
+    public List<Course> Courses { get; set; } = new List<Course>();
+
 }
 
 
