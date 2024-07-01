81bf79ff619cf0ae8d9e45e65c7fab8c55a34cc3
Karl Ericsson <lexicon@services.karlericsson.com>
2024-07-01 11:01:36 +0200

    fix(api): Add mappings and relations mainly for document

--- Lexicon.Api/Dtos/DocumentDtos/DocumentDto.cs
+++ Lexicon.Api/Dtos/DocumentDtos/DocumentDto.cs
@@ -24,6 +24,8 @@ public class DocumentDto
 
     public int UserId { get; set; }
 
-    public int ModuleId { get; set; }
+    public int? CourseId { get; set; }
+    public int? ModuleId { get; set; }
+    public int? ActivityId { get; set; }
 
 }
