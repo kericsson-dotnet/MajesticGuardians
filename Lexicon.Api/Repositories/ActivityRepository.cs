daf2610284b777409e9728d3498d161b3094fe06
Karl Ericsson <lexicon@services.karlericsson.com>
2024-07-01 11:18:30 +0200

    feat(api): Add ActivityRepository

---
+++ Lexicon.Api/Repositories/ActivityRepository.cs
@@ -0,0 +1,27 @@
+﻿using Lexicon.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lexicon.Api.Repositories;
+
+public class ActivityRepository(DbContext context) : CrudRepository<Activity>(context), IActivityRepository
+{
+    public virtual async Task<IEnumerable<Activity>> GetAllAsync()
+    {
+        return await _context.Set<Activity>()
+            .Include(a => a.Documents)
+            .ToListAsync();
+    }
+
+    public virtual async Task<Activity> GetAsync(object id)
+    {
+        if (id is int activityId)
+        {
+            return await _context.Set<Activity>()
+                       .Include(a => a.Documents)
+                       .FirstOrDefaultAsync(a => a.ActivityId == activityId) ??
+                   throw new InvalidOperationException($"{typeof(Activity).Name} Id {id} not found.");
+        }
+
+        throw new ArgumentException("Invalid ID type");
+    } 
+}
--- Lexicon.Api/Repositories/UnitOfWork.cs
+++ Lexicon.Api/Repositories/UnitOfWork.cs
@@ -7,19 +7,17 @@ public class UnitOfWork : IUnitOfWork
 {
     private readonly LexiconLmsContext _context;
 
-    public ICrudRepository<Activity> Activities { get; private set; }
-
     public UnitOfWork(LexiconLmsContext context)
     {
         _context = context;
-        Activities = new CrudRepository<Activity>(_context);
     }
 
-    public IDocumentRepository Documents => new DocumentRepository(_context);
-
-    public ICourseRepository Courses => new CourseRepository(_context);
     public IUserRepository Users => new UserRepository(_context);
+    public ICourseRepository Courses => new CourseRepository(_context);
     public IModuleRepository Modules => new ModuleRepository(_context);
+    public IActivityRepository Activities => new ActivityRepository(_context);
+
+    public IDocumentRepository Documents => new DocumentRepository(_context);
     
 
     public async Task SaveAsync()
