feddc2fb336359f7c0921404e8f4279089a08ae3
98emre <emre-98@live.se>
2024-07-01 22:16:42 +0200

    feat: added validation for user model

--- Lexicon.Frontend/Models/User.cs
+++ Lexicon.Frontend/Models/User.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace Lexicon.Frontend.Models;
 
 public enum UserRole
@@ -10,12 +12,30 @@ public enum UserRole
 public class User
 {
     public int UserId { get; set; }
+
+    [Required(ErrorMessage = "Förnamn är obligatoriskt")]
+    [StringLength(100, ErrorMessage = "Förnamn kan inte vara mer än 100 tecken")]
+    [MinLength(2, ErrorMessage = "Förnamn måste vara minst 2 tecken")]
     public string FirstName { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Efternamn är obligatoriskt")]
+    [StringLength(100, ErrorMessage = "Efternamn kan inte vara mer än 100 tecken")]
+    [MinLength(2, ErrorMessage = "Efternamn måste vara minst 2 tecken")]
     public string LastName { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Epost är obligatoriskt")]
+    [StringLength(100, ErrorMessage = "Epost kan inte vara mer än 100 tecken")]
+    [EmailAddress(ErrorMessage = "Ogiltig e-postadress")]
     public string Email { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Lösenord är obligatoriskt")]
+    [StringLength(100, ErrorMessage = "Lösenord kan inte vara mer än 100 tecken")]
+    [MinLength(4, ErrorMessage = "Lösenord måste vara minst 4 tecken")]
     public string Password { get; set; } = string.Empty;
 
+    [Required(ErrorMessage = "Roll är obligatoriskt")]
     public UserRole Role { get; set; } 
+
     public List<Document> Documents { get; set; } = [];
     public List<int> DocumentIds { get; set; } = [];
     public List<Course> Courses { get; set; } = [];
