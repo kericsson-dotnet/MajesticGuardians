3a03ca1ffcc18870b1184ec1c974e379ea741afa
98emre <emre-98@live.se>
2024-07-01 23:52:34 +0200

    feat: added validation for course

--- Lexicon.Frontend/Models/Course.cs
+++ Lexicon.Frontend/Models/Course.cs
@@ -1,14 +1,27 @@
-﻿namespace Lexicon.Frontend.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lexicon.Frontend.Models;
 
 public class Course
 {
     public int CourseId { get; set; }
+
+    [Required(ErrorMessage = "Namn är obligatoriskt")]
+    [StringLength(100, MinimumLength = 4, ErrorMessage = "Namnet måste vara mellan 4 och 100 tecken")]
     public string Name { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Beskrivning är obligatorisk")]
+    [StringLength(500, MinimumLength = 10, ErrorMessage = "Beskrivningen måste vara mellan 10 och 500 tecken")]
     public string Description { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Start datum är obligatoriskt")]
+    public DateTime StartDate { get; set; } = DateTime.Now;
+
+    [Required(ErrorMessage = "Slut datums är obligatoriskt")]
+    public DateTime EndDate { get; set; }
+
     public List<User> Users { get; set; } = [];
     public List<int> UserIds { get; set; } = [];
     public List<int> DocumentIds { get; set; } = [];
     public List<int> ModuleIds { get; set; } = [];
-    public DateTime StartDate { get; set; } = DateTime.Now;
-    public DateTime EndDate { get; set; }
 }
