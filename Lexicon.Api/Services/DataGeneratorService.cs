607491b789d2da1f9a291eac6810f666382bde29
FelixEdenborgh <Felix@edenborgh.se>
2024-07-01 14:31:21 +0200

    fixed something that dident join the last push

--- Lexicon.Api/Services/DataGeneratorService.cs
+++ Lexicon.Api/Services/DataGeneratorService.cs
@@ -118,6 +118,22 @@ public class DataGeneratorService(IUnitOfWork unitOfWork)
                 StartDate = faker.Date.Past(),
                 EndDate = faker.Date.Future(),
                 // Documents = null
+            },
+
+            // The Complete Python Bootcamp From Zero to Hero in Python
+            new Module
+            {
+                Name = "Fel och Undantag i Python",
+                StartDate = faker.Date.Past(),
+                EndDate = faker.Date.Future(),
+                // Documents = null
+            },
+            new Module
+            {
+                Name = "Objektorienterad Programmering i Python",
+                StartDate = faker.Date.Past(),
+                EndDate = faker.Date.Future(),
+                // Documents = null
             }
         };
 
