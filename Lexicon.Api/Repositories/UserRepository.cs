4a4b1a07f37d9c7c4ecdb43455cab83b1915d9bc
Karl Ericsson <lexicon@services.karlericsson.com>
2024-07-01 11:28:29 +0200

    fix(api): Update includes in UserRepository

--- Lexicon.Api/Repositories/UserRepository.cs
+++ Lexicon.Api/Repositories/UserRepository.cs
@@ -6,6 +6,27 @@ namespace Lexicon.Api.Repositories;
 public class UserRepository(DbContext context) : CrudRepository<User>(context), IUserRepository
 
 {
+    public virtual async Task<IEnumerable<User>> GetAllAsync()
+    {
+        return await _context.Set<User>()
+            .Include(u => u.Courses)
+            .Include(u => u.Documents)
+            .ToListAsync();
+    }
+
+    public virtual async Task<User> GetAsync(object id)
+    {
+        if (id is int userId)
+        {
+            return await _context.Set<User>()
+                       .Include(u => u.Courses)
+                       .Include(u => u.Documents)
+                       .FirstOrDefaultAsync(u => u.UserId == userId) ??
+                   throw new InvalidOperationException($"{typeof(User).Name} Id {id} not found.");
+        }
+        throw new ArgumentException("Invalid ID type");
+    }
+
     public async Task<User?> ValidateCredentialsAsync(string email, string password)
     {
         var normalizedEmail = email.ToLowerInvariant();
