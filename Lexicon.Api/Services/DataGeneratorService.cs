84579b0224302e9b058ab5940b3fc2d893550d94
FelixEdenborgh <Felix@edenborgh.se>
2024-07-01 15:56:54 +0200

    Fix: updated info for module

--- Lexicon.Api/Services/DataGeneratorService.cs
+++ Lexicon.Api/Services/DataGeneratorService.cs
@@ -108,6 +108,7 @@ public class DataGeneratorService(IUnitOfWork unitOfWork)
             new Module
             {
                 Name = "C# Lexicon LMS",
+                Description = "Ett Learning Management System (LMS) är en mjukvaruplattform som används för administration, dokumentation, spårning, rapportering, automatisering och leverans av utbildningskurser, utbildningsprogram eller inlärnings- och utvecklingsprogram. Ett LMS gör det möjligt för utbildningsinstitutioner, företag och organisationer att effektivt hantera och distribuera utbildningsinnehåll till sina användare.",
                 StartDate = faker.Date.Past(),
                 EndDate = faker.Date.Future(),
                 // Documents = null
@@ -115,6 +116,7 @@ public class DataGeneratorService(IUnitOfWork unitOfWork)
             new Module
             {
                 Name = "Industriell maskinpark –\r\nBlazor",
+                Description = "Ett maskinparkshanteringssystem är en mjukvaruplattform som används för att övervaka och hantera en flotta av maskiner och fordon. Systemet gör det möjligt för företag att effektivt spåra, underhålla och optimera användningen av sina maskiner och fordon, vilket resulterar i förbättrad driftseffektivitet och minskade driftkostnader.",
                 StartDate = faker.Date.Past(),
                 EndDate = faker.Date.Future(),
                 // Documents = null
@@ -122,6 +124,7 @@ public class DataGeneratorService(IUnitOfWork unitOfWork)
             new Module
             {
                 Name = "Blazor och Grundläggande LINQ i C#",
+                Description = "Ett maskinparkshanteringssystem är en mjukvaruplattform som används för att övervaka och hantera en flotta av maskiner och fordon. Systemet gör det möjligt för företag att effektivt spåra, underhålla och optimera användningen av sina maskiner och fordon, vilket resulterar i förbättrad driftseffektivitet och minskade driftkostnader. Genom att använda Blazor för frontend-utveckling och LINQ för datamanipulation i C# blir utvecklingen både modern och effektiv.",
                 StartDate = faker.Date.Past(),
                 EndDate = faker.Date.Future(),
                 // Documents = null
@@ -131,6 +134,7 @@ public class DataGeneratorService(IUnitOfWork unitOfWork)
             new Module
             {
                 Name = "Objektorienterad Programmering i Python",
+                Description = "Objektorienterad programmering (OOP) är ett programmeringsparadigm som organiserar programvara genom att kombinera data och funktionalitet i enheter som kallas \"objekt\". Python är ett kraftfullt programmeringsspråk som stöder OOP, vilket gör det möjligt för utvecklare att skapa strukturerad och återanvändbar kod. OOP i Python underlättar design och utveckling av komplexa system genom att modulera problem i hanterbara och logiska enheter.",
                 StartDate = faker.Date.Past(),
                 EndDate = faker.Date.Future(),
                 // Documents = null
@@ -138,6 +142,7 @@ public class DataGeneratorService(IUnitOfWork unitOfWork)
             new Module
             {
                 Name = "Fel och Undantag i Python",
+                Description = "Fel och undantag är en viktig del av programmering som hjälper utvecklare att hantera oförutsedda situationer och buggar som kan uppstå under körning av ett program. I Python finns det ett robust system för att fånga och hantera undantag, vilket förbättrar programmets tillförlitlighet och användarupplevelse.",
                 StartDate = faker.Date.Past(),
                 EndDate = faker.Date.Future(),
                 // Documents = null
@@ -145,6 +150,8 @@ public class DataGeneratorService(IUnitOfWork unitOfWork)
             new Module
             {
                 Name = "Fel och Undantag i Python 2 och Python Selenium",
+                Description = "Fel och undantag är viktiga koncept inom programmering, som hjälper utvecklare att hantera oväntade situationer och buggar som kan uppstå under körning av ett program. Python 2 har ett system för att fånga och hantera undantag, vilket förbättrar programmets tillförlitlighet och användarupplevelse.\n" +
+                "Selenium är ett kraftfullt verktyg för web automatisering. Det används för att automatisera webbläsare för att simulera en användares interaktioner med webbapplikationer. Selenium kan integreras med Python för att skriva testskript och automatiseringsskript.",
                 StartDate = faker.Date.Past(),
                 EndDate = faker.Date.Future(),
                 // Documents = null
