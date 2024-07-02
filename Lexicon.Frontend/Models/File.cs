d9a34029e4d70b585d8127a24cbaf18cf05d11cf
98emre <emre-98@live.se>
2024-07-02 10:31:05 +0200

    feat: added file model

---
+++ Lexicon.Frontend/Models/File.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lexicon.Frontend.Models
+{
+    public class File
+    {
+        [Required(ErrorMessage = "Namn är obligatoriskt.")]
+        [StringLength(100, ErrorMessage = "Namnet får inte vara längre än 100 tecken.")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Beskrivning är obligatorisk.")]
+        [StringLength(500, ErrorMessage = "Beskrivningen får inte vara längre än 500 tecken.")]
+        public string Description { get; set; } = string.Empty;
+
+        public string UserId { get; set; } = string.Empty;
+        
+        public string? ModuleId { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Att välja en fil är obligatoriskt")]
+        public IReadOnlyList<IBrowserFile> Attachments { get; set; }
+    }
+}
